a2b692fa9713879a3b074093a826d2235180f398
Yuval Nachman <yuviwx@gmail.com>
2022-03-06 18:33:03 +0200

    change Redirect_Uri to netlify

--- src/util/Spotify.js
+++ src/util/Spotify.js
@@ -1,6 +1,6 @@
 
 const Client_Id  = '2be4c4098af9489fbbb7e257335b3fe0';
-const Redirect_Uri = "http://localhost:3000/";
+const Redirect_Uri = "https://plus-ultra-playlist.netlify.app";
 
 let accessToken;
 
